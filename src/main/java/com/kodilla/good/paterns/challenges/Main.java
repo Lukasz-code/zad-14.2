d3628223653244241033bd598c60f77007fc0b22
unknown <lukaszschweda@gmail.com>
2020-01-25 20:54:39 +0100

    13.1

---
+++ src/main/java/com/kodilla/good/paterns/challenges/Main.java
@@ -0,0 +1,14 @@
+package com.kodilla.good.paterns.challenges;
+
+import java.util.stream.Collectors;
+
+public class Main {
+    public static void main(String[] args) {
+        MovieStore movieStore = new MovieStore();
+        String moviesString = movieStore.getMovies().entrySet().stream()
+                .map(entry -> entry.getKey() + ": " + entry.getValue())
+                .collect(Collectors.joining(" ! "," "," "));
+
+        System.out.println(moviesString);
+    }
+}
